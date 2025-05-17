09cf02d0c2301b86b843f0bd34ffe8273fd948c9
moktar939 <moktarm243@gmail.com>
2025-05-17 23:18:01 +0300

    spearate chaining

--- src/SeparateChaining.java
+++ src/SeparateChaining.java
@@ -1,5 +1,19 @@
-import java.util.LinkedList;
 
-public class SeparateChaining {
+public class SeparateChainingHashTable {
+        private linkedlist[] table;
+        private int size;
 
-}
+        public SeparateChainingHashTable(int size) {
+            this.size = size;
+            table = new linkedlist[size];
+            for (int i = 0; i < size; i++) {
+                table[i] = new linkedlist();
+            }
+        }
+
+        public void insert(String key) {
+            int index = HashUtils.hash(key, size);
+            table[index].insertAtEnd(key);
+        }
+    }
+
